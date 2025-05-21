42aac9fbd6d34d9562b8484dc98172a094940d63
private-4t <abie113@private-4t.com>
2025-05-21 11:02:15 +0300

    Fix plugin database installation

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025052100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025052101;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041900;        // Requires this Moodle version (4.0).
 $plugin->component = 'enrol_upayment';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;   // This is considered as stable code.
